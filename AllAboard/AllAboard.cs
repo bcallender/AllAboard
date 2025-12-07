c973f52b2d28a034b0f0b1fc29e5d81f2b40404c
Brandon Callender <bcallender@users.noreply.github.com>
2025-12-06 16:47:13 -0800

    trains do, in fact, need ai

--- AllAboard/AllAboard.cs
+++ AllAboard/AllAboard.cs
@@ -37,7 +37,7 @@ namespace AllAboard
                 (uint)m_AllAboardSettings.BusMaxDwellDelaySlider;
             World.DefaultGameObjectInjectionWorld.GetOrCreateSystemManaged<TransportTrainAISystem>().Enabled = false;
             World.DefaultGameObjectInjectionWorld.GetOrCreateSystemManaged<TransportCarAISystem>().Enabled = false;
-            updateSystem.UpdateAt<System.Patched.PatchedTransportCarAISystem>(SystemUpdatePhase.GameSimulation);
+            updateSystem.UpdateAt<System.Patched.PatchedTransportTrainAISystem>(SystemUpdatePhase.GameSimulation);
             updateSystem.UpdateAt<System.Patched.PatchedTransportCarAISystem>(SystemUpdatePhase.GameSimulation);
             log.Info("Completed Replacement of Base Train/CarAI Systems.");
             log.InfoFormat("Bus Max Dwell Time: {0}", PublicTransportBoardingHelper.BusMaxAllowedMinutesLate.Data);
